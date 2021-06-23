bed3ae26c2ecd889b062495cadc7744e0e8ff03f
Dimas Emeraldo <dimas05014@gmail.com>
2021-06-23 13:57:14 +0700

    storedb

--- application/views/welcome_message.php
+++ application/views/welcome_message.php
@@ -142,11 +142,11 @@
               </div>
               <!-- /.card-header -->
               <!-- form start -->
-              <form>
+              <form action="" method="post">
                 <div class="card-body">
                   <div class="form-group">
                     <label>Perguruan Tinggi</label>
-                    <select name="univ" class="form-control select2bs4" style="width: 100%;" required>
+                    <select name="perguruan_tinggi" class="form-control select2bs4" style="width: 100%;" required>
                       <option selected="selected"></option>
                       <option value="UNIVERSITAS SULTAN AGENG TIRTAYASA">UNIVERSITAS SULTAN AGENG TIRTAYASA</option>
                       <option value="UNIVERSITAS AIRLANGGA">UNIVERSITAS AIRLANGGA</option>
@@ -227,7 +227,7 @@
                   <div class="form-group">
                   <label>Tanggal Kadaluarsa</label>
                     <div class="input-group">
-                      <input name="tanggal" type="date" class="form-control"
+                      <input name="kadaluarsa" type="date" class="form-control"
                       min="2021-01-01" required>
                       <span class="validity"></span>
                     </div>
@@ -236,9 +236,29 @@
                 <!-- /.card-body -->
 
                 <div class="card-footer">
-                  <button type="submit" class="btn btn-primary">Submit</button>
+                  <button name="proses" type="submit" class="btn btn-primary">Submit</button>
                 </div>
               </form>
+              <?php
+                $koneksi = mysqli_connect("localhost","root","","akreditasi");
+
+                if(isset($_POST['proses'])){
+                mysqli_query($koneksi, "insert into banpt set  
+                perguruan_tinggi = '$_POST[perguruan_tinggi]',
+                prodi = '$_POST[prodi]',
+                strata = '$_POST[strata]',
+                wilayah = '$_POST[wilayah]',
+                no_sk = '$_POST[no_sk]',
+                tahun_sk = '$_POST[tahun_sk]',
+                peringkat = '$_POST[peringkat]',
+                kadaluarsa = '$_POST[kadaluarsa]'");
+
+                echo "Data Peserta Baru Telah Disimpan";
+                
+
+                }
+
+                ?>
             </div>
     </section>
     <!-- /.content -->
@@ -250,13 +270,13 @@
     <div class="float-right d-none d-sm-inline-block">
       <b>Version</b> 3.1.0
     </div>
-    </footer>
+  </footer>
 
-<!-- Control Sidebar -->
-<aside class="control-sidebar control-sidebar-dark">
-  <!-- Control sidebar content goes here -->
-</aside>
-<!-- /.control-sidebar -->
+  <!-- Control Sidebar -->
+  <aside class="control-sidebar control-sidebar-dark">
+    <!-- Control sidebar content goes here -->
+  </aside>
+  <!-- /.control-sidebar -->
 </div>
 <!-- ./wrapper -->
 
@@ -266,7 +286,7 @@
 <script src="asset/plugins/jquery-ui/jquery-ui.min.js"></script>
 <!-- Resolve conflict in jQuery UI tooltip with Bootstrap tooltip -->
 <script>
-$.widget.bridge('uibutton', $.ui.button)
+  $.widget.bridge('uibutton', $.ui.button)
 </script>
 <!-- Bootstrap 4 -->
 <script src="asset/plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
@@ -294,172 +314,5 @@ $.widget.bridge('uibutton', $.ui.button)
 <script src="asset/dist/js/demo.js"></script>
 <!-- AdminLTE dashboard demo (This is only for demo purposes) -->
 <script src="asset/dist/js/pages/dashboard.js"></script>
-<!-- Select2 -->
-<script src="asset/plugins/select2/js/select2.full.min.js"></script>
-<!-- Bootstrap4 Duallistbox -->
-<script src="asset/plugins/bootstrap4-duallistbox/jquery.bootstrap-duallistbox.min.js"></script>
-<!-- InputMask -->
-<script src="asset/plugins/moment/moment.min.js"></script>
-<script src="asset/plugins/inputmask/jquery.inputmask.min.js"></script>
-<!-- date-range-picker -->
-<script src="asset/plugins/daterangepicker/daterangepicker.js"></script>
-<!-- bootstrap color picker -->
-<script src="asset/plugins/bootstrap-colorpicker/js/bootstrap-colorpicker.min.js"></script>
-<!-- Tempusdominus Bootstrap 4 -->
-<script src="asset/plugins/tempusdominus-bootstrap-4/js/tempusdominus-bootstrap-4.min.js"></script>
-<!-- Bootstrap Switch -->
-<script src="asset/plugins/bootstrap-switch/js/bootstrap-switch.min.js"></script>
-<!-- BS-Stepper -->
-<script src="asset/plugins/bs-stepper/js/bs-stepper.min.js"></script>
-<!-- dropzonejs -->
-<script src="asset/plugins/dropzone/min/dropzone.min.js"></script>
-<!-- date-range-picker -->
-<script src="asset/plugins/daterangepicker/daterangepicker.js"></script>
-<!-- AdminLTE App -->
-<script src="asset/dist/js/adminlte.min.js"></script>
-<!-- datepicker -->
-<script src="<?php echo base_url('asset/bootstrap-datepicker/dist/js/bootstrap-datepicker.min.js');?>" rel="stylesheet"></script>
-<!-- Page specific script -->
-<script>
-$(function () {
-  //Initialize Select2 Elements
-  $('.select2').select2()
-
-  //Initialize Select2 Elements
-  $('.select2bs4').select2({
-    theme: 'bootstrap4'
-  })
-
-  //Datemask dd/mm/yyyy
-  $('#datemask').inputmask('dd/mm/yyyy', { 'placeholder': 'dd/mm/yyyy' })
-  //Datemask2 mm/dd/yyyy
-  $('#datemask2').inputmask('mm/dd/yyyy', { 'placeholder': 'mm/dd/yyyy' })
-  //Money Euro
-  $('[data-mask]').inputmask()
-
-  //Date picker
-  $('#reservationdate').datetimepicker({
-      format: 'L'
-  });
-
-  //Date and time picker
-  $('#reservationdatetime').datetimepicker({ icons: { time: 'far fa-clock' } });
-
-  //Date range picker
-  $('#reservation').daterangepicker()
-  //Date range picker with time picker
-  $('#reservationtime').daterangepicker({
-    timePicker: true,
-    timePickerIncrement: 30,
-    locale: {
-      format: 'MM/DD/YYYY hh:mm A'
-    }
-  })
-  //Date range as a button
-  $('#daterange-btn').daterangepicker(
-    {
-      ranges   : {
-        'Today'       : [moment(), moment()],
-        'Yesterday'   : [moment().subtract(1, 'days'), moment().subtract(1, 'days')],
-        'Last 7 Days' : [moment().subtract(6, 'days'), moment()],
-        'Last 30 Days': [moment().subtract(29, 'days'), moment()],
-        'This Month'  : [moment().startOf('month'), moment().endOf('month')],
-        'Last Month'  : [moment().subtract(1, 'month').startOf('month'), moment().subtract(1, 'month').endOf('month')]
-      },
-      startDate: moment().subtract(29, 'days'),
-      endDate  : moment()
-    },
-    function (start, end) {
-      $('#reportrange span').html(start.format('MMMM D, YYYY') + ' - ' + end.format('MMMM D, YYYY'))
-    }
-  )
-
-  //Timepicker
-  $('#timepicker').datetimepicker({
-    format: 'LT'
-  })
-
-  //Bootstrap Duallistbox
-  $('.duallistbox').bootstrapDualListbox()
-
-  //Colorpicker
-  $('.my-colorpicker1').colorpicker()
-  //color picker with addon
-  $('.my-colorpicker2').colorpicker()
-
-  $('.my-colorpicker2').on('colorpickerChange', function(event) {
-    $('.my-colorpicker2 .fa-square').css('color', event.color.toString());
-  })
-
-  $("input[data-bootstrap-switch]").each(function(){
-    $(this).bootstrapSwitch('state', $(this).prop('checked'));
-  })
-
-})
-// BS-Stepper Init
-document.addEventListener('DOMContentLoaded', function () {
-  window.stepper = new Stepper(document.querySelector('.bs-stepper'))
-})
-
-// DropzoneJS Demo Code Start
-Dropzone.autoDiscover = false
-
-// Get the template HTML and remove it from the doumenthe template HTML and remove it from the doument
-var previewNode = document.querySelector("#template")
-previewNode.id = ""
-var previewTemplate = previewNode.parentNode.innerHTML
-previewNode.parentNode.removeChild(previewNode)
-
-var myDropzone = new Dropzone(document.body, { // Make the whole body a dropzone
-  url: "/target-url", // Set the url
-  thumbnailWidth: 80,
-  thumbnailHeight: 80,
-  parallelUploads: 20,
-  previewTemplate: previewTemplate,
-  autoQueue: false, // Make sure the files aren't queued until manually added
-  previewsContainer: "#previews", // Define the container to display the previews
-  clickable: ".fileinput-button" // Define the element that should be used as click trigger to select files.
-})
-
-myDropzone.on("addedfile", function(file) {
-  // Hookup the start button
-  file.previewElement.querySelector(".start").onclick = function() { myDropzone.enqueueFile(file) }
-})
-
-// Update the total progress bar
-myDropzone.on("totaluploadprogress", function(progress) {
-  document.querySelector("#total-progress .progress-bar").style.width = progress + "%"
-})
-
-myDropzone.on("sending", function(file) {
-  // Show the total progress bar when upload starts
-  document.querySelector("#total-progress").style.opacity = "1"
-  // And disable the start button
-  file.previewElement.querySelector(".start").setAttribute("disabled", "disabled")
-})
-
-// Hide the total progress bar when nothing's uploading anymore
-myDropzone.on("queuecomplete", function(progress) {
-  document.querySelector("#total-progress").style.opacity = "0"
-})
-
-// Setup the buttons for all transfers
-// The "add files" button doesn't need to be setup because the config
-// `clickable` has already been specified.
-document.querySelector("#actions .start").onclick = function() {
-  myDropzone.enqueueFiles(myDropzone.getFilesWithStatus(Dropzone.ADDED))
-}
-document.querySelector("#actions .cancel").onclick = function() {
-  myDropzone.removeAllFiles(true)
-}
-// DropzoneJS Demo Code End
-$(document).ready(function() {
-  $('.datepicker').datepicker({
-    format: 'yyyy-mm-dd',
-    autoclose: true,
-    todayHighlight: true,
-  });
-});
-</script>
 </body>
 </html>
